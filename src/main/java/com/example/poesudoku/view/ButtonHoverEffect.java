0a312c0eccc5d600d2b85482b176c664c54a3d14
carlosk4 <lorenzohinestroza44@gmail.com>
2026-05-26 18:17:08 -0500

    blue, smooth

--- src/main/java/com/example/poesudoku/view/ButtonHoverEffect.java
+++ src/main/java/com/example/poesudoku/view/ButtonHoverEffect.java
@@ -1,5 +1,6 @@
 package com.example.poesudoku.view;
 
+import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
@@ -8,24 +9,25 @@ import javafx.util.Duration;
 
 public class ButtonHoverEffect implements ButtonHoverable {
 
+    private static final Duration HOVER_DURATION = Duration.millis(130);
+
     @Override
     public void applyButtonHoverEffect(Button button) {
-        button.setOnMouseEntered(e -> {
-            Timeline tl = new Timeline(
-                    new KeyFrame(Duration.millis(150),
-                            new KeyValue(button.opacityProperty(), 0.7)
-                    )
-            );
-            tl.play();
-        });
+        button.setOnMouseEntered(e -> animate(button, 1.02, -1));
+        button.setOnMouseExited(e -> animate(button, 1.0, 0));
+        button.setOnMousePressed(e -> animate(button, 0.98, 0));
+        button.setOnMouseReleased(e -> animate(button, 1.02, -1));
+    }
 
-        button.setOnMouseExited(e -> {
-            Timeline tl = new Timeline(
-                    new KeyFrame(Duration.millis(150),
-                            new KeyValue(button.opacityProperty(), 1.0)
-                    )
-            );
-            tl.play();
-        });
+    private void animate(Button button, double scale, double translateY) {
+        Timeline timeline = new Timeline(
+                new KeyFrame(
+                        HOVER_DURATION,
+                        new KeyValue(button.scaleXProperty(), scale, Interpolator.EASE_OUT),
+                        new KeyValue(button.scaleYProperty(), scale, Interpolator.EASE_OUT),
+                        new KeyValue(button.translateYProperty(), translateY, Interpolator.EASE_OUT)
+                )
+        );
+        timeline.play();
     }
 }
